156832d2a80c905dd8620afb578216fef87b1924
Sergey Ragimov <seriy_ragimov@mail.ru>
2022-07-11 22:04:00 +0300

    Добавлен Response

---
+++ yatube/posts/views.py
@@ -0,0 +1,12 @@
+from http.client import HTTPResponse
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse('<h1>Главная страница</h1>')
+
+
+def group_posts(request, name):
+    return HttpResponse(f'Посты {name}')
