c0774146f53fe5ab3544603b7c8323008fadbfdd
Sergey Ragimov <seriy_ragimov@mail.ru>
2022-07-14 22:07:51 +0300

    Добавлен app_name и имена path

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'posts'
 
 urlpatterns = [
-    path('', views.index),
-    path('group/<slug:name>/', views.group_posts)
+    path('', views.index, name='main'),
+    path('group_list.html', views.group_posts, name='group')
 ]
