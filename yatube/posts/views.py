79773dad4109d8020fc2f8a47ed9e061c626a9f7
Sergey Ragimov <seriy_ragimov@mail.ru>
2022-07-14 22:08:39 +0300

    Добавлен context

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,9 +4,18 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    template = 'posts/index.html'
-    return render(request, template)
+    template_main = 'posts/index.html'
+    text = "Это главная страница проекта Yatube"
+    context = {
+        'text': text,
+    }
+    return render(request, template_main, context)
 
 
-def group_posts(request, name):
-    return HttpResponse(f'Посты {name}')
+def group_posts(request):
+    template_group = 'posts/group_list.html'
+    text = 'Здесь будет информация о группах проекта Yatube'
+    context = {
+        'text': text,
+    }
+    return render(request, template_group, context)
